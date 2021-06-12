a5120b68c8482a0a471129ae5cbb619bf1cb3fee
tantakan <shoma@tantakan.net>
2021-06-12 16:17:46 +0900

    [MOD] responsive design

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,12 @@ const IndexPage = () => {
       <title>Kastagnette</title>
       <div className="h-screen flex flex-wrap flex-row">
         <div className="flex flex-wrap flex-col content-end bg-gray-50 w-1/2">
+
           <div className="h-screen flex flex-wrap flex-row content-center">
-            <div className="flex flex-col mx-4">
+            <div className="flex flex-col w-36 md:w-64">
               <img className="self-center w-24 h-24 md:w-36 md:h-36 rounded-full border-solid border-4 border-gray-200" src="/shoma.jpeg" />
               <p className="font-sans text-center text-gray-700 text-xl md:text-4xl mt-2">Shoma Saito</p>
-              <p className="font-sans text-center text-gray-600 font-light md:text-lg">@tantakan</p>
+              <p className="font-sans text-center text-gray-600 font-light md:text-lg text-sm">@tantakan</p>
               <div className="flex flex-wrap flex-row self-center mt-8 md:mt-16">
                 <a className="mx-2" target="_blank" href="https://twitter.com/tantakan_">
                   <img className="w-6 md:w-8" src="twitter_logo_icon_black.svg" />
@@ -27,26 +28,29 @@ const IndexPage = () => {
               </div>
             </div>
           </div>
+
         </div>
         <div className="bg-gray-800  w-1/2">
+
           <div className="h-screen flex flex-wrap flex-row content-center">
-            <div className="flex flex-col mx-4">
+            <div className="flex flex-col w-36 md:w-64">
               <img className="self-center w-24 h-24 md:w-36 md:h-36 rounded-full border-solid border-4 border-gray-600" src="/eri.jpeg" />
               <p className="font-sans text-center text-gray-200 text-xl md:text-4xl mt-2">Eri Maeda</p>
-              <p className="font-sans text-center text-gray-400 font-light md:text-lg">@music431per</p>
+              <p className="font-sans text-center text-gray-400 font-light md:text-lg text-sm">@music431per</p>
               <div className="flex flex-wrap flex-row self-center mt-8 md:mt-16">
                 <a className="mx-2" target="_blank" href="https://twitter.com/music431per">
-                  <img className="w-8 md:w-8" src="twitter_logo_icon_white.svg" />
+                  <img className="w-6 md:w-8" src="twitter_logo_icon_white.svg" />
                 </a>
                 <a className="mx-2" target="_blank" href="https://www.facebook.com/music431per">
-                  <img className="w-8 md:w-8" src="facebook_logo_icon_white.svg" />
+                  <img className="w-6 md:w-8" src="facebook_logo_icon_white.svg" />
                 </a>
                 <a className="mx-2" target="_blank" href="https://www.instagram.com/music431per">
-                  <img className="w-8 md:w-8" src="instagram_logo_icon_white.svg" />
+                  <img className="w-6 md:w-8" src="instagram_logo_icon_white.svg" />
                 </a>
               </div>
             </div>
           </div>
+
         </div>
       </div>
     </main>
