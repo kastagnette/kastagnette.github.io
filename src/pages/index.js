40be3abffbb5c1eefa14970e575c47b6a5ff53e8
tantakan <shoma@tantakan.net>
2021-06-12 15:08:04 +0900

    [MOD] profile contents

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,8 @@ const IndexPage = () => {
         <div className="h-screen flex flex-wrap flex-row content-center">
           <div className="flex flex-col w-72">
             <img className="self-center w-36 h-36 rounded-full border-solid border-4 border-gray-200" src="/shoma.jpeg" />
-            <p className="font-sans text-center my-2 text-gray-700 text-4xl">Shoma Saito</p>
-            <p className="font-sans text-center text-gray-600 font-light text-2xl">Engineer</p>
-            <p className="font-sans text-center text-gray-600 font-light text-2xl">Love Tea!</p>
+            <p className="font-sans text-center text-gray-700 text-4xl mt-2">Shoma Saito</p>
+            <p className="font-sans text-center text-gray-600 font-light text-lg">@tantakan</p>
             <div className="flex flex-wrap flex-row self-center mt-16">
               <a className="mx-2" target="_blank" href="https://twitter.com/tantakan_">
                 <img className="w-8" src="twitter_logo_icon_black.svg" />
@@ -31,9 +30,8 @@ const IndexPage = () => {
         <div className="h-screen flex flex-wrap flex-row content-center">
           <div className="flex flex-col w-72">
             <img className="self-center w-36 h-36 rounded-full border-solid border-4 border-gray-600" src="/eri.jpeg" />
-            <p className="font-sans text-center my-2 text-gray-200 text-4xl">Eri Maeda</p>
-            <p className="font-sans text-center text-gray-400 font-light text-2xl">Engineer</p>
-            <p className="font-sans text-center text-gray-400 font-light text-2xl">Love Coffee!</p>
+            <p className="font-sans text-center text-gray-200 text-4xl mt-2">Eri Maeda</p>
+            <p className="font-sans text-center text-gray-400 font-light text-lg">@music431per</p>
             <div className="flex flex-wrap flex-row self-center mt-16">
               <a className="mx-2" target="_blank" href="https://twitter.com/music431per">
                 <img className="w-8" src="twitter_logo_icon_white.svg" />
