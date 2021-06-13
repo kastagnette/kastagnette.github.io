cd2751348df91e3f133b1ed18bf190f051f23676
tantakan <shoma@tantakan.net>
2021-06-13 11:23:41 +0900

    [MOD] name

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ const IndexPage = () => {
           <div className="min-h-screen flex flex-wrap flex-row content-center">
             <div className="flex flex-col w-36 md:w-64">
               <img className="self-center w-24 h-24 md:w-36 md:h-36 rounded-full border-solid border-4 border-gray-600" alt="eri" src="/eri.jpeg" />
-              <p className="font-sans text-center text-gray-200 text-xl md:text-4xl mt-2">Eri Maeda</p>
+              <p className="font-sans text-center text-gray-200 text-xl md:text-4xl mt-2">Eri Saito</p>
               <p className="font-sans text-center text-gray-400 font-light md:text-lg text-sm">@music431per</p>
               <div className="flex flex-wrap flex-row self-center mt-8 md:mt-16">
                 <a className="mx-2" target="_blank" rel="noreferrer" href="https://twitter.com/music431per">
